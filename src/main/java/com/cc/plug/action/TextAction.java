a97b2577836386dd278b00868fedba9c86718f0e
cc <wyswydx@gmail.com>
2024-01-29 14:12:55 +0800

    1. initial idea plug file

---
+++ src/main/java/com/cc/plug/action/TextAction.java
@@ -0,0 +1,19 @@
+package com.cc.plug.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
+
+public class TextAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        SelectionModel model = editor.getSelectionModel();
+        String text = model.getSelectedText();
+        System.out.println(text);
+    }
+}
